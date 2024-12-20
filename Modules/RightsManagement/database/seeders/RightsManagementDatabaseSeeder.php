f48b8eb1939f5462284ef4d58b33bddf1cf0f118
Jemish Harsora <jemish.harsora@globalpay.com>
2024-12-20 16:12:14 +0530

    Added nwidart/laravel-modules and wikimedia/composer-merge-plugin packages

---
+++ Modules/RightsManagement/database/seeders/RightsManagementDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\RightsManagement\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class RightsManagementDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/RightsManagement/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('rightsmanagement::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('rightsmanagement.name') !!}</p>
+@endsection
